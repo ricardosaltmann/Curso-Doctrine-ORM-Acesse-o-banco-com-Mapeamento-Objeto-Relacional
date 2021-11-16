2925495b3267e2eaa14bdc40ffb3c079a4cf3f36
Ricardo Altmann <ricardoaltmann@outlook.com.br>
2021-11-16 13:30:51 -0300

    criando a entidade Curso

--- commands/buscar-alunos.php
+++ commands/buscar-alunos.php
@@ -1,6 +1,7 @@
 <?php
 
 use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Telefone;
 use Alura\Doctrine\Helper\EntityManagerFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -14,14 +15,14 @@ $alunoRepository = $entityManager->getRepository(Aluno::class);
 $alunoList = $alunoRepository->findAll();
 
 foreach ($alunoList as $aluno) {
-    echo "ID: {$aluno->getId()}\nNome: {$aluno->getNome()}\n\n";
+    $telefones = $aluno
+        ->getTelefones()
+        ->map(function (Telefone $telefone) {
+            return $telefone->getNumero();
+        })
+        ->toArray();
+    echo "ID: {$aluno->getId()}\nNome: {$aluno->getNome()}\n";
+    echo "Telefones: " . implode(', ', $telefones);
+
+    echo "\n\n";
 }
-
-$nico = $alunoRepository->find(4);
-echo $nico->getNome() . "\n\n";
-
-$sergioLopes = $alunoRepository->findBy([
-    'nome' => 'Sergio Lopes'
-]);
-
-var_dump($sergioLopes);
